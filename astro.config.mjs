87cb0693d7ab9d045cb56671bad53b76403c87c0
Nicolazzzz <166757623+Nicolazzzz@users.noreply.github.com>
2026-02-17 23:44:28 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aether-astro-theme.netlify.app/',
+  site: 'https://nicolaz2.github.io',
   integrations: [tailwind(), react(), sitemap()],
 });
